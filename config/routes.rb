e86f6c1ada4ba339f298e340b4028ac1e99e86d7
kohei.sugo <same.go.0813@gmail.com>
2018-03-14 14:06:28 +0000

    Revert "kadai user login"
    This reverts commit 316a41bd2784dd260868e3b285acdb95eeb8f199.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+  root to: "toppages#index"
   
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  
-  resources :tasks
+#  resources :tasks
 end
